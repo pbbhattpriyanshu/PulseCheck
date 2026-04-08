c651eae72f80ef226f78a52437a791bf8fbcc735
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-04-08 13:52:14 +0530

    feat: initialize Express server with MongoDB connection and project configuration

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
+import dbConnect from "./src/Config/database.config.js";
 
 const PORT = process.env.PORT || 3000;
+dbConnect();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 const app = express();
 
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 //Demo route
 app.get("/", (req, res) => {
     res.send("PulseCheck Server is running");
